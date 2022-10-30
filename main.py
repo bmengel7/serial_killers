7d587b54df59c0184609137c51428b27a741bd44
Brie Mengel <bmengel7@gmail.com>
2022-10-30 18:17:26 -0400

    added serial killer data

---
+++ main.py
@@ -0,0 +1,15 @@
+serial_killers = [
+    ["Jack the Ripper", 1887, 1888, 5 ],
+    ["Jeffrey Dahmer", 1974, 1991, 17 ],
+    ["Harold Shipman", 1972, 1998, 250 ],
+    ["John Wayne Gacy", 1978, 1994, 33],
+    ["H.H. Holmes", 1983, 1986, 30], 
+
+]
+headers = [ 
+    "Name", 
+    "From",
+    "To",
+    "Victim Count",
+
+] 
