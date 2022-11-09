4b5f86a1273dc9235240e46a0a8c5d267c17b772
Brie Mengel <bmengel7@gmail.com>
2022-11-08 22:32:43 -0500

    combined men and women list

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 serial_killers = [
-    ["Jack the Ripper", 1887, 1888, 5, "Knife Wound, Multilation" ],
-    ["Jeffrey Dahmer", 1974, 1991, 17, "Strangulation"],
-    ["Harold Shipman", 1972, 1998, 250, "Morphine Overdose" ],
-    ["John Wayne Gacy", 1978, 1994, 33, "Asphyxiation with garrot"],
-    ["H.H. Holmes", 1983, 1986, 30, "Hanged"], 
-    ['Luis Garavito', 1992, 1999, 147, "Stabbing, Mutilation"],
-    ['Henry Lee Lucas', 1946, 1983, 41 "Stabbing"]
+    ["Jack the Ripper", 1887, 1888, 5, "Knife Wound, Multilation", "Male"],
+    ["Jeffrey Dahmer", 1974, 1991, 17, "Strangulation", "Male"],
+    ["Harold Shipman", 1972, 1998, 250, "Morphine Overdose", "Male" ],
+    ["John Wayne Gacy", 1978, 1994, 33, "Asphyxiation with garrot", "Male"],
+    ["H.H. Holmes", 1983, 1986, 30, "Hanged", "Male"], 
+    ['Luis Garavito', 1992, 1999, 147, "Stabbing, Mutilation", "Male"],
+    ['Henry Lee Lucas', 1946, 1983, 41, "Stabbing", "Male"],
+    ['Aileen Wuornos', 1989, 1990, 7, "Gun Shot", "Female"],
+    ['Judias Buenoano', 1971, 1980, 3, "Arsenic Poisoning", "Female"],
+    ['Juana Barraza', 2002, 2006, 40, "Strangulation", "Female"],
+    ["Jane Toppan or Jolly Jane", 1895, 1901, 31, "Poisoning", "Female"],
+    ['Gesche Gottfried', 1813, 1827, 15, "Poisoning", "Female"],
+    ['Amelia Dyer', 1869, 1896, 400, "Poison, Starvation, Strangulation","Female"],
+    ['kristen Gilbert', 1995, 1996, 4, "Stimulant Injections", "Female"],
+    ['Nannie Doss', 1920, 1954, 11, "Rat Poison", "Female"],
+    ['Dorotha Puente', 1982, 1988, 9, "Drug Overdose", "Female"],
+    ['Miyuki Ishikawa', 1944, 1948, 169, 'Neglect', "Female"],
     
 
 ]
@@ -16,42 +26,23 @@ headers = [
     "From",
     "To",
     "Victim Count",
-    "Modus Operandi"
-    "Gender"
+    "Modus Operandi",
+    "Gender",
 
 ] 
-sk = pd.DataFrame(serial_killers, columns=headers)
-
-serial_killer_ladies = [
-    ['Aileen Wuornos', 1989, 1990, 7, "Gun Shot"]
-    ['Judias Buenoano', 1971, 1980, 3, "Arsenic Poisoning"]
-    ['Juana Barraza', 2002, 2006, 40, "Strangulation"]
-    ["Jane Toppan or Jolly Jane", 1895, 1901, 31, "Poisoning"]
-    ['Gesche Gottfried', 1813, 1827, 15, "Poisoning"],
-    ['Amelia Dyer', 1869, 1896, 400, "Poison, Starvation, Strangulation"],
-    ['kristen Gilbert', 1995, 1996, 4 "Stimulant Injections"],
-    ['Nannie Doss', 1920, 1954, 11, "Rat Poison"],
-    ['Dorotha Puente', 1982, 1988, 9, "Drug Overdose" ],
-    ['Miyuki Ishikawa', 1944, 1948, 169, 'Neglect']
-
-]
-
-headers2 = [ 
-    "Name", 
-    "From",
-    "To",
-    "Victim Count",
-    "Modus Operandi"
-
-] 
-skw = pd.DataFrame(serial_killer_ladies, columns=headers2)
-
-sk['duration'] = 1 + sk['To'].astype(int) - sk['From'].astype(int)
+skm = pd.DataFrame(serial_killers, columns=headers)
 
 
-sk['rate'] = sk['Victim Count'] /  sk['duration']
+skm['duration'] = 1 + skm['To'].astype(int) - skm['From'].astype(int)
+
+
+skm['rate'] = skm['Victim Count'] /  skm['duration']
 
 
 # plot rates by killer (simple for example)
 
-sk['rate'].plot(kind="bar")
+skm['rate'].plot(kind="bar")
+
+
+# plot rates by killer (simple for example)
+
